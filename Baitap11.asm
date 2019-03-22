5a6c67dc523275e8ceb472c5fdaa36abf45a45a2
= <=>
2019-03-22 20:44:50 +0700

    no

--- Baitap11.asm
+++ Baitap11.asm
@@ -6,9 +6,9 @@
     tb1 db 'So A 16bit: $'
     tb2 db 10, 13, 'So B 16bit: $'
     tb3 db 10, 13, 'A + B =    $'
-    tb4 db 10, 13, 'A - B = $'
-    tb5 db 10, 13, 'A || B = $'
-    tb6 db 10, 13, 'A ^ B = $'
+    tb4 db 10, 13, 'A - B =     $'
+    tb5 db 10, 13, 'A || B =   $'
+    tb6 db 10, 13, 'A ^ B =    $'
 MAIN PROC
     mov ax, @data
     mov ds,ax
@@ -47,8 +47,8 @@ Lap2:
     lea dx, tb3
     int 21h
     
-    mov bx,b1
-    add bx,b2
+    mov bx,b2
+    add bx,b1
     mov cx,16
     mov dx, 30h
     adc dx,0
@@ -63,7 +63,22 @@ Lap3:
     adc dl,0
     mov ah,2
     int 21h
-    loop Lap3
+    loop Lap3  
+    
+    mov ah,9
+    lea dx,tb4
+    int 21h
+    
+    mov bx,b1
+    sub bx,b2
+    mov cx,16
+Lap4:
+    mov dl,30h
+    shl bx,1
+    adc dl,0
+    mov ah,2
+    int 21h
+    loop Lap4
     
         mov ah,4ch
         int 21h
