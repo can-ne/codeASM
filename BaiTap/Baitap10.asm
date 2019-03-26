e00badd9e4bda52568a5a07d360376a57bf19fe8
Nezzt87 <canabcde@gmail.com>
2019-03-26 15:53:24 +0700

    chinh sua

--- BaiTap/Baitap10.asm
+++ BaiTap/Baitap10.asm
@@ -6,7 +6,10 @@
 .data
     so db 10,0,10 dup($)
     muoi db 10
-    xuong db 10,13,'$'
+    tb1 db 10,13,'A: $'
+    tb2 db 10,13,'B: $'
+    tb3 db 10,13,'A/B: $'
+    tb4 db 10,13,'A*B: $'
     b1 dw 0
     b2 dw 0
 .code
@@ -14,12 +17,19 @@
         mov ax,@data
         mov ds,ax
         
+        mov ah,9
+        lea dx,tb1
+        int 21h
+        
         mov ah,0Ah
         lea dx,so
         int 21h
+        
         mov ah,9
-        lea dx,xuong
+        lea dx,tb2 
         int 21h
+        
+        xor cx,cx
         lea si,so + 2
         mov cl,[so+1]
         Lap:
@@ -49,15 +59,40 @@
             mov b2,ax
             inc si
             loop Lap2
+        
         mov dx,b1
         mov ax,b2
+        
         xor cx,cx
         chia:
             sub dx,ax
             inc cx
             cmp dx,ax
             jae chia 
+        
+        mov ax,cx
+        xor cx,cx 
+        
+        mov ah,9
+        lea dx,tb3
+        int 21h
+        
+        Lapchia1:
+            xor dx,dx
+            div muoi
             
+            add ah,30h
+            mov dl,ah
+            push dx
+            inc cx
+            xor ah,ah
+            cmp ax,0
+            jne Lapchia1
+        Hienthi1:
+            pop dx
+            mov ah,2 
+            int 21h
+            loop Hienthi1    
         
         xor cx,cx
         xor dx,dx 
@@ -66,7 +101,29 @@
         Nhan:
             add dx,b1
             Loop Nhan     
+        mov ax,dx
+        xor cx,cx   
         
+        lea dx,tb4
+        mov ah,9
+        int 21h
+        
+        Lapchia2:
+            xor dx,dx
+            div muoi
+            
+            add ah,30h
+            mov dl,ah
+            push dx
+            inc cx
+            xor ah,ah
+            cmp ax,0
+            jne Lapchia2
+        Hienthi2:
+            pop dx
+            mov ah,2 
+            int 21h
+            loop Hienthi2
         mov ah,4Ch
         int 21h
     main endp
